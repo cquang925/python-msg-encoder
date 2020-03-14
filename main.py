67c48a2db5dee2bd130e3692025beb1d934a4143
cquang925 <52224686+cquang925@users.noreply.github.com>
2020-03-14 07:17:10 -0500

    Update main.py
    Added try/except blocks. Added justification to 'Main Menu'

--- main.py
+++ main.py
@@ -6,41 +6,47 @@ from decoder import user_msg, upload_msg
 import randomizer
 import pyinputplus as pyip
 
-while True:
-    print('~~~~~~~~~~ Main Menu ~~~~~~~~~~')
-    choice = pyip.inputMenu(['Encode Message', 'Decode Message', 'Create Key', 'View/Print Key', 'Quit'], numbered=True)
-
     if choice == 'Encode Message':
         print()
-        randomizer.import_key()
+        try:
+            randomizer.import_key()
+        except FileNotFoundError:
+            print("Please create key.")
         encoder()
 
     elif choice == 'Decode Message':
         print()
-        randomizer.import_key()
-        reply = pyip.inputChoice(['Key Message', 'Import Message'])
-        if reply == 'Key Message':
-            user_msg()
-        elif reply == 'Import Message':
-            upload_msg()
+        try:
+            randomizer.import_key()
+        except FileNotFoundError:
+            print("Please create key.")
+        else:
+            reply = pyip.inputChoice(['Key Message', 'Import Message'])
+            if reply == 'Key Message':
+                user_msg()
+            elif reply == 'Import Message':
+                upload_msg()
 
     elif choice == 'Create Key':
         randomizer.randomize()
         randomizer.save_key()
-        print('Key has been created successfully')
-        view_key = input('Would you like to view the key?(y/n)\n')
-        if view_key == 'y':
-            print(randomizer.characters, '\n')
-        elif view_key == 'n':
+        print('\n Key has been created successfully')
+        view_key = pyip.inputYesNo('Would you like to view the key?(y/n)\n')
+        if view_key == 'yes':
+            print(randomizer.characters + '\n')
+        elif view_key == 'no':
             print('Returning to Main Menu \n')
 
     elif choice == 'View/Print Key':
-        randomizer.import_key()
-        randomizer.view_key()
+        try:
+            randomizer.import_key()
+        except FileNotFoundError:
+            print("Unable to find key file. \n")
+        else:
+            randomizer.view_key()
 
     elif choice == 'Quit':
         print('Have a good day! Good-bye.')
         break
 
-# todo create try catch blocks to capture errors
 # todo check to see if character dictionary is empty

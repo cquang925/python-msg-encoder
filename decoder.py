64efe3165448ad68b2b51a015243e85a1ccf0059
cquang925 <52224686+cquang925@users.noreply.github.com>
2020-03-14 07:22:30 -0500

    Update decoder.py
    Added code to check if coded_msg is empty. Added Try/Except to decoder()

--- decoder.py
+++ decoder.py
@@ -9,7 +9,11 @@ coded_msg = []
 
 # has user input a coded message to decode
 def user_msg():
-    coded_msg.clear()
+    if len(coded_msg) != 0:
+        coded_msg.clear()
+    else:
+        pass
+
     user_input = (input('Type in coded message (separate by space): '))
     x = user_input.split(" ")  # takes user_input and separates string into items in list
     for i in x:  # reiterates through items in list and appends to coded_msg
@@ -19,7 +23,11 @@ def user_msg():
 
 # imports message from 'coded message.txt' and runs through decoder
 def upload_msg():
-    coded_msg.clear()
+    if len(coded_msg) != 0:
+        coded_msg.clear()
+    else:
+        pass
+    
     with open("coded message.txt", 'r') as file:  # opens file
         for num in file:  # iterates through file
             num = num.strip()  # removes '\n' from each line
@@ -32,11 +40,14 @@ def upload_msg():
 def decoder():
     decoded_text = []
     for num in coded_msg:  # decodes the text
-        decoded_text.append(list(characters.keys())[list(characters.values()).index(num)])
+        try:
+            decoded_text.append(list(characters.keys())[list(characters.values()).index(num)])
+        except ValueError:
+            print(num + ' is not in key')
 
     print(*decoded_text, sep='',)  # prints decoded message and removes quotations
     print()
 
 
 # todo make upload_msg more dynamic and able to import through long line
-# todo create try/catch for user input errors and key not found errors
+# TODO create code to look for coded_message.txt in current directory
